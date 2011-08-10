5e7c60f4bbde0c8f035fd49790ece25702e6c5fb
Quintus <sutniuq@gmx.net>
2011-08-10 10:45:19 +0200

    Ensured that the formatter if required alone gets the rdoc gem

--- lib/rdoc/markup/to_latex.rb
+++ lib/rdoc/markup/to_latex.rb
@@ -20,6 +20,7 @@ along with RDoc PDF LaTeX; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =end
 
+gem "rdoc"
 require "rdoc/markup/formatter"
 require "rdoc/markup/inline"
 
